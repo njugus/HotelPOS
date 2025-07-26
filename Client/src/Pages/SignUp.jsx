455bcea1e527a4ef1446189b9060266db6574fda
njugus <kayvohnjuguna@gmail.com>
2025-07-26 12:16:31 -0700

    adds sign up code for user sign up

---
+++ Client/src/Pages/SignUp.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { TextField, Button } from "@mui/material";
+import AuthFormWrapper from "../Components/AuthFormWrapper";
+
+const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email").required("Required"),
+      password: Yup.string().min(6, "At least 6 characters").required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log("Signup form submitted:", values);
+      // Send to backend here
+    },
+  });
+
+  return (
+    <AuthFormWrapper title="Create Account">
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="Full Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button fullWidth variant="contained" type="submit" style={{ backgroundColor: "#31511E" }}>
+          Sign Up
+        </Button>
+      </form>
+    </AuthFormWrapper>
+  );
+};
+
+export default Signup;
